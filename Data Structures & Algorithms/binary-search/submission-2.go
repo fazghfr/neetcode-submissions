168179849541792bc5742b013d7bcc00f812b4e0
Ahmad Fauzan Alghifari <96367502+fazghfr@users.noreply.github.com>
2026-07-09 10:31:31 +0700

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.go
@@ -0,0 +1,22 @@
+func search(nums []int, target int) int {
+	left := 0 
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := left + (right - left) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return -1
+}
+
+// binary search approach
+// mid calculation mid = left + (right - left) / 2
+// if mid is bigger than target right = mid - 1
+// else left = mid + 1
