a3d4af4d1b6a7275208c71848a354f9548f067f4
Ahmad Fauzan Alghifari <96367502+fazghfr@users.noreply.github.com>
2026-09-24 15:05:35 +0700

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.go
@@ -0,0 +1,23 @@
+func tribonacci(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 || n == 2 {
+		return 1
+	}
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 1
+	dp[2] = 1
+	if n < 3 {
+		return dp[n]
+	}
+
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+	}
+
+	return dp[n]
+}
+
+
+
