e6a0b0dd4d7d239c4794c3e4bef570fb3e7d0dd8
Ahmad Fauzan Alghifari <96367502+fazghfr@users.noreply.github.com>
2026-09-26 20:57:30 +0700

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.go
@@ -0,0 +1,27 @@
+
+func rob1(nums []int) int {
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	if len(nums) < 2 {
+		return dp[0]
+	}
+	dp[1] = max(dp[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+	}
+
+	return dp[len(nums) - 1]
+}
+
+
+func rob(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	return max(rob1(nums[1:]), rob1(nums[:len(nums)-1]))
+}
+
+
+
+
+
